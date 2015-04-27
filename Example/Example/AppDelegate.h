d5f8db51583bb03485ac2cc6c6683bb766a685f6
Adenike Olatunji <d10125267@ast-1027-m21.ict.ad.dit.ie>
2015-04-27 21:29:04 +0100

    Labtest

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Adenike Olatunji on 27/04/2015.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Adenike Olatunji on 27/04/2015.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
